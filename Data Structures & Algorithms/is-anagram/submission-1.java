f4521cd7d7489cd60c83c21beb7d7dfb286e7777
Surati Deep Navinbhai <cse240840131192@gmail.com>
2026-09-08 11:58:31 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            } else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (map2.containsKey(t.charAt(i))) {
+                map2.put(t.charAt(i), map2.get(t.charAt(i)) + 1);
+            } else {
+                map2.put(t.charAt(i), 1);
+            }
+        }
+
+        return map.equals(map2);
+    }
+}
